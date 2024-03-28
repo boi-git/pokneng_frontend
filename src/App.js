ae2b7197790a3fe3aba879a769c7ff09fa644572
boi-git <adhwa.aizat23@gmail.com>
2024-03-28 22:27:11 +0800

    [CarouselCard] Use new carosuel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Body from './components/Body';
-import HorizontalCard from './components/HorizontalCard';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Body from "./components/Body";
+import CarouselCardContainer from "./components/Bottom Nav/CarouselCardContainer ";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="h-lvh w-screen flex flex-col relative">
+    <div className="h-lvh w-screen flex flex-col relative overflow-hidden">
       <Navbar />
       <Body />
-      <HorizontalCard />
+      <CarouselCardContainer />
     </div>
   );
 }
