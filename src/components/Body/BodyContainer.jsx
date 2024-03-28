ea24e89c373d5b76b1d4c35b3ca2954386835692
boi-git <adhwa.aizat23@gmail.com>
2024-03-28 22:52:42 +0800

    [Body > Content] Create Body > Content

---
+++ src/components/Body/BodyContainer.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Content from "./Content";
+
+const BodyContainer = () => {
+  return (
+    <div className="h-screen overflow-hidden ">
+      <Content></Content>
+    </div>
+  );
+};
+
+export default BodyContainer;
+
+//!todo
+//1. make the image turn into simple video of them doing live. it will be a video after 2/3 seconds
